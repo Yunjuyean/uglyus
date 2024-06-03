1139c6cd8b467ffd8eba90bcfc44f9e80e1fbb97
Yeonjuu35 <yeonjuu35@gmail.com>
2024-06-03 15:32:43 +0900

    [2.0.0] 네비게이션 DB 아키텍처 자바스크립트 작성

---
+++ js/hd.js
@@ -0,0 +1,94 @@
+const hd_yys ={
+    logo : {
+        Text : "uglyus",
+        href : "/uglyus/",
+        img : "/uglyus/img/img_yys/svg/ugly_us_logo.svg",
+        cls : ["me-3"]
+    },
+    gnb : [
+        {
+        Text : "우리의 미션",
+        href : "us_misson.html",
+        cls : ["nav-mission1 me-5 gnb_li rel","d-block gnb_a","gnb_ul abs","gnb_ul_li"],
+        gnb_ul_li : [
+            {
+                Text : "미션",
+                href : "mission.html"
+            },
+            {
+                Text : "소싱철학",
+                href : "sourcing.html"
+            }
+        ]
+    },
+    {
+        Text : "정기배송",
+        href : "delivery.html",
+        cls : ["nav-mission2 me-5 gnb_li rel","d-block gnb_a","gnb_ul abs","gnb_ul_li"],
+        gnb_ul_li : [
+            {
+                Text : "후기",
+                href : "review.html"
+            },
+            {
+                Text : "혜택",
+                href : "benefit.html"
+            },
+            {
+                Text : "이용안내",
+                href : "guide.html"
+            }
+        ]
+    },
+    {
+        Text : "싱싱마켓",
+        href : "market.html",
+        cls : ["nav-mission3 me-5 gnb_li rel","d-block gnb_a","gnb_ul abs","gnb_ul_li"],
+        gnb_ul_li : [
+            {
+                Text : "추천상품",
+                href : "recommend.html"
+            },
+            {
+                Text : "인기상품",
+                href : "best.html"
+            },
+            {
+                Text : "대기중인상품",
+                href : "stand_by.html"
+            }
+        ]
+    },
+    {
+        Text : "커뮤니티",
+        href : "community.html",
+        cls : ["nav-mission4 me-5 gnb_li rel","d-block gnb_a","gnb_ul abs","gnb_ul_li"],
+        gnb_ul_li : [
+            {
+                Text : "오늘의레시피",
+                href : "recipe.html"
+            },
+            {
+                Text : "채소이야기",
+                href : "vege_story"
+            },
+            {
+                Text : "어글리어스밥풀루언서",
+                href : "babfluencer.html"
+            }
+        ]
+    }
+    ],
+    util : [
+        {
+            Text : "로그인",
+            href : "login.html",
+            cls : ["d-flex px-0 my-0","me-3 font_login","nav-link font_login"]
+        },
+        {
+            Text : "회원가입",
+            href : "consumer.html",
+            cls : ["d-flex px-0 my-0","font_login","nav-link me-0 font_login"]
+        }
+    ]
+} 
